e60862a48c5d1796c39253a83249d2925fd706e7
harshilkareliya <kareliyaharshil2111@gmail.com>
2024-03-04 14:57:38 +0530

    task completed

---
+++ 01 JAVASCRIPT/Weekly Project 6/JS.js
@@ -0,0 +1,125 @@
+var all_recipe = JSON.parse(localStorage.getItem("recipe_data")) || []
+console.log(all_recipe)
+
+function showRecipe(){
+    document.getElementById("show_recipe").innerHTML = ""
+    all_recipe.map((recipes,index)=>{
+        let main_sec = document.createElement("div")
+        main_sec.setAttribute("id","main_sec")
+        let name = document.createElement("div")
+        name.setAttribute("id","recipe_name")
+        name.append(recipes.title)
+        let Ingred = document.createElement("div")
+        Ingred.setAttribute("id","recipe_Ingred") 
+        Ingred.append(recipes.ingredients)
+        let Instruct = document.createElement("div")
+        Instruct.setAttribute("id","recipe_Instruct") 
+        Instruct.append(recipes.instructions)
+        let cuise = document.createElement("div")
+        cuise.setAttribute("id","recipe_cuise") 
+        cuise.append(recipes.cuisine)
+        let edit_sec = document.createElement("div")
+        edit_sec.setAttribute("id","edit_sec")
+        let edit = document.createElement("i")
+        edit.addEventListener("click",()=>{
+            let change_recipe = all_recipe.filter(ft => ft.id === index+1)
+            
+            edit_recipe(change_recipe) 
+            showRecipe()
+        })
+        edit.setAttribute("class","bi bi-pencil-square") 
+        let remove = document.createElement("i")
+        remove.setAttribute("class","bi bi-x-lg") 
+        remove.addEventListener("click",()=>{
+            delete_recipe(all_recipe,index)
+            showRecipe()
+        })
+        edit_sec.append(edit,remove)
+        main_sec.append(name,Ingred,Instruct,cuise,edit_sec)
+        document.getElementById("show_recipe").append(main_sec)
+    })
+
+}
+showRecipe()
+
+function delete_recipe(all_recipe,index){
+    all_recipe.splice(index,1)
+    localStorage.setItem("recipe_data",JSON.stringify(all_recipe))
+
+    return all_recipe
+}
+
+function edit_recipe(recipe){
+    let change = prompt()
+    recipe[0].title.innerHTML = change
+    
+    return recipe
+}
+
+function filter_recipe(){
+    let filter_type = document.getElementById("filter_cuisine").value
+    if(filter_type == 'not selected'){
+        showRecipe()
+    }
+    else {
+        let filtered_recipe = all_recipe.filter(ft => ft.cuisine === filter_type)
+        displayFiltered_recipe(filtered_recipe)
+    }
+}
+
+function displayFiltered_recipe(all_recipe){
+    document.getElementById("show_recipe").innerHTML = ""
+    all_recipe.map((recipes,index)=>{
+        let main_sec = document.createElement("div")
+        main_sec.setAttribute("id","main_sec")
+        let name = document.createElement("div")
+        name.setAttribute("id","recipe_name")
+        name.append(recipes.title)
+        let Ingred = document.createElement("div")
+        Ingred.setAttribute("id","recipe_Ingred") 
+        Ingred.append(recipes.ingredients)
+        let Instruct = document.createElement("div")
+        Instruct.setAttribute("id","recipe_Instruct") 
+        Instruct.append(recipes.instructions)
+        let cuise = document.createElement("div")
+        cuise.setAttribute("id","recipe_cuise") 
+        cuise.append(recipes.cuisine)
+        let edit_sec = document.createElement("div")
+        edit_sec.setAttribute("id","edit_sec")
+        let edit = document.createElement("i")
+        edit.addEventListener("click",()=>{
+            let change_recipe = all_recipe.filter(ft => ft.id === index+1)
+            edit_recipe(change_recipe) 
+            showRecipe()
+        })
+        edit.setAttribute("class","bi bi-pencil-square") 
+        let remove = document.createElement("i")
+        remove.setAttribute("class","bi bi-x-lg") 
+        remove.addEventListener("click",()=>{
+            delete_recipe(all_recipe,index)
+            showRecipe()
+        })
+        edit_sec.append(edit,remove)
+        main_sec.append(name,Ingred,Instruct,cuise,edit_sec)
+        document.getElementById("show_recipe").append(main_sec)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function getdata(){
+    var all_recipe = JSON.parse(localStorage.getItem("recipe_data")) || []
+}
+
+function setData(changes){
+    localStorage.setItem("recipe_data",JSON.stringify(changes))
+}
---
+++ 01 JAVASCRIPT/Weekly Project 6/header.js
@@ -0,0 +1,12 @@
+function header(){
+    let nav = `<div id="header">
+    <div><img src="img/logo.jpg" alt="" id="logo"></div>
+    <nav id="navbar">
+        <li><a href="index.html">Add Recipe</a></li>
+        <li><a href="recipe.html" target="_blank">My Recipe</a></li>
+    </nav>
+    </div>`
+
+    document.getElementById("header").innerHTML = nav
+}
+export{header}
